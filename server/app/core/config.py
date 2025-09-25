59acf738284e84e2777ccbbd3e893333314b8861
Nabin Shrestha <30395905+nabinstha1234@users.noreply.github.com>
2025-09-25 01:47:32 +0000

    feat: initialzed fast api project

---
+++ server/app/core/config.py
@@ -0,0 +1,36 @@
+import secrets
+import warnings
+
+from typing import Annotated, Any, Literal
+
+from pydantic import (
+    AnyUrl,
+    BeforeValidator,
+    EmailStr,
+    HttpUrl,
+    PostgresDsn,
+    computed_field,
+    model_validator,
+)
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    FRONTEND_HOST: str = "http://localhost:5173"
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+
+    BACKEND_CORS_ORIGINS: Annotated[
+        list[AnyUrl] | str, BeforeValidator(parse_cors)
+    ] = []
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def all_cors_origins(self) -> list[str]:
+        return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
+            self.FRONTEND_HOST
+        ]
+
+    API_V1_STR: str = "/api/v1"
+
+    PROJECT_NAME: str
+
+settings = Settings()  # type: ignore
